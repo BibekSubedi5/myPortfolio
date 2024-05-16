7242a975aadd1884f6346ffb7835b436379c6cfb
Bibek subedi <subedi.bibek164@gmail.com>
2024-05-16 16:43:36 +0545

    Add/Correct build script and configurationsssz

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-import "@fontsource/roboto";
-import "@fontsource/outfit";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
