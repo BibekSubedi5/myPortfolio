91c28450dabbcfb095ab0b7bea088edc12a1acda
Bibek subedi <subedi.bibek164@gmail.com>
2024-05-16 16:01:03 +0545

    Fix configuration issues

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   build: {
     rollupOptions: {
       external: ['@fontsource/outfit', '@fontsource/roboto']
-    },
+    }
   },
-  
   server: {
-    // Allow serving files from node_modules
     fs: {
       allow: ['..'] // Allow serving files from parent directories
     }
   },
   plugins: [react()],
-  css:{
-    modules:{
-      localsConvention:"camelCase",
+  css: {
+    modules: {
+      localsConvention: "camelCase"
     }
   }
-})
+});
